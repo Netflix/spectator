677964ce16758c01bcd28410e1464b5ec2b5b3c1
Brian Harrington <brharrington@gmail.com>
2016-02-02 08:40:30 -0800

    add bucket helpers for quantities other than time
    Update BucketFunctions so it provides helpers for working with
    quantities other than time. The most commonly requested has been
    bytes which will automatically generate labels using powers of
    1024. There is also a helper for other arbitrary quantities that
    uses the decimal metric prefixes.

--- spectator-api/src/main/java/com/netflix/spectator/api/histogram/BucketFunctions.java
+++ spectator-api/src/main/java/com/netflix/spectator/api/histogram/BucketFunctions.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.LongFunction;
+import java.util.function.LongUnaryOperator;
 
 /**
  * Helpers for creating bucketing functions.
@@ -26,83 +27,141 @@ import java.util.function.LongFunction;
 public final class BucketFunctions {
 
   /**
-   * Predefined formatters used to create the bucket labels.
+   * Predefined time formatters used to create the bucket labels.
    */
-  static final List<ValueFormatter> FORMATTERS = new ArrayList<>();
+  static final List<ValueFormatter> TIME_FORMATTERS = new ArrayList<>();
+
+  /**
+   * Predefined binary formatters used to create bucket labels. For more information see
+   * <a href="https://en.wikipedia.org/wiki/Binary_prefix">binary prefixes wiki</a>.
+   */
+  static final List<ValueFormatter> BINARY_FORMATTERS = new ArrayList<>();
+
+  /**
+   * Predefined decimal formatters used to created bucket labels. For more informations see
+   * <a href="https://en.wikipedia.org/wiki/Metric_prefix">metric prefixes wiki</a>.
+   */
+  static final List<ValueFormatter> DECIMAL_FORMATTERS = new ArrayList<>();
 
   static {
-    FORMATTERS.add(fmt(TimeUnit.NANOSECONDS.toNanos(10),     1, "ns",  TimeUnit.NANOSECONDS));
-    FORMATTERS.add(fmt(TimeUnit.NANOSECONDS.toNanos(100),    2, "ns",  TimeUnit.NANOSECONDS));
-    FORMATTERS.add(fmt(TimeUnit.MICROSECONDS.toNanos(1),     3, "ns",  TimeUnit.NANOSECONDS));
-    FORMATTERS.add(fmt(TimeUnit.MICROSECONDS.toNanos(8),     4, "ns",  TimeUnit.NANOSECONDS));
-    FORMATTERS.add(fmt(TimeUnit.MICROSECONDS.toNanos(10),    1, "us",  TimeUnit.MICROSECONDS));
-    FORMATTERS.add(fmt(TimeUnit.MICROSECONDS.toNanos(100),   2, "us",  TimeUnit.MICROSECONDS));
-    FORMATTERS.add(fmt(TimeUnit.MILLISECONDS.toNanos(1),     3, "us",  TimeUnit.MICROSECONDS));
-    FORMATTERS.add(fmt(TimeUnit.MILLISECONDS.toNanos(8),     4, "us",  TimeUnit.MICROSECONDS));
-    FORMATTERS.add(fmt(TimeUnit.MILLISECONDS.toNanos(10),    1, "ms",  TimeUnit.MILLISECONDS));
-    FORMATTERS.add(fmt(TimeUnit.MILLISECONDS.toNanos(100),   2, "ms",  TimeUnit.MILLISECONDS));
-    FORMATTERS.add(fmt(TimeUnit.SECONDS.toNanos(1),          3, "ms",  TimeUnit.MILLISECONDS));
-    FORMATTERS.add(fmt(TimeUnit.SECONDS.toNanos(8),          4, "ms",  TimeUnit.MILLISECONDS));
-    FORMATTERS.add(fmt(TimeUnit.SECONDS.toNanos(10),         1, "s",   TimeUnit.SECONDS));
-    FORMATTERS.add(fmt(TimeUnit.SECONDS.toNanos(100),        2, "s",   TimeUnit.SECONDS));
-    FORMATTERS.add(fmt(TimeUnit.MINUTES.toNanos(8),          3, "s",   TimeUnit.SECONDS));
-    FORMATTERS.add(fmt(TimeUnit.MINUTES.toNanos(10),         1, "min", TimeUnit.MINUTES));
-    FORMATTERS.add(fmt(TimeUnit.MINUTES.toNanos(100),        2, "min", TimeUnit.MINUTES));
-    FORMATTERS.add(fmt(TimeUnit.HOURS.toNanos(8),            3, "min", TimeUnit.MINUTES));
-    FORMATTERS.add(fmt(TimeUnit.HOURS.toNanos(10),           1, "h",   TimeUnit.HOURS));
-    FORMATTERS.add(fmt(TimeUnit.HOURS.toNanos(100),          2, "h",   TimeUnit.HOURS));
-    FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(8),             1, "h",   TimeUnit.HOURS));
-    FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(10),            1, "d",   TimeUnit.DAYS));
-    FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(100),           2, "d",   TimeUnit.DAYS));
-    FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(1000),          3, "d",   TimeUnit.DAYS));
-    FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(10000),         4, "d",   TimeUnit.DAYS));
-    FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(100000),        5, "d",   TimeUnit.DAYS));
-    FORMATTERS.add(fmt(Long.MAX_VALUE,                       6, "d",   TimeUnit.DAYS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.NANOSECONDS.toNanos(10),     1, "ns",  TimeUnit.NANOSECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.NANOSECONDS.toNanos(100),    2, "ns",  TimeUnit.NANOSECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MICROSECONDS.toNanos(1),     3, "ns",  TimeUnit.NANOSECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MICROSECONDS.toNanos(8),     4, "ns",  TimeUnit.NANOSECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MICROSECONDS.toNanos(10),    1, "us",  TimeUnit.MICROSECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MICROSECONDS.toNanos(100),   2, "us",  TimeUnit.MICROSECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MILLISECONDS.toNanos(1),     3, "us",  TimeUnit.MICROSECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MILLISECONDS.toNanos(8),     4, "us",  TimeUnit.MICROSECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MILLISECONDS.toNanos(10),    1, "ms",  TimeUnit.MILLISECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MILLISECONDS.toNanos(100),   2, "ms",  TimeUnit.MILLISECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.SECONDS.toNanos(1),          3, "ms",  TimeUnit.MILLISECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.SECONDS.toNanos(8),          4, "ms",  TimeUnit.MILLISECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.SECONDS.toNanos(10),         1, "s",   TimeUnit.SECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.SECONDS.toNanos(100),        2, "s",   TimeUnit.SECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MINUTES.toNanos(8),          3, "s",   TimeUnit.SECONDS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MINUTES.toNanos(10),         1, "min", TimeUnit.MINUTES));
+    TIME_FORMATTERS.add(fmt(TimeUnit.MINUTES.toNanos(100),        2, "min", TimeUnit.MINUTES));
+    TIME_FORMATTERS.add(fmt(TimeUnit.HOURS.toNanos(8),            3, "min", TimeUnit.MINUTES));
+    TIME_FORMATTERS.add(fmt(TimeUnit.HOURS.toNanos(10),           1, "h",   TimeUnit.HOURS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.HOURS.toNanos(100),          2, "h",   TimeUnit.HOURS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(8),             1, "h",   TimeUnit.HOURS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(10),            1, "d",   TimeUnit.DAYS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(100),           2, "d",   TimeUnit.DAYS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(1000),          3, "d",   TimeUnit.DAYS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(10000),         4, "d",   TimeUnit.DAYS));
+    TIME_FORMATTERS.add(fmt(TimeUnit.DAYS.toNanos(100000),        5, "d",   TimeUnit.DAYS));
+    TIME_FORMATTERS.add(fmt(Long.MAX_VALUE,                       6, "d",   TimeUnit.DAYS));
     // TimeUnit.NANOSECONDS.toDays(java.lang.Long.MAX_VALUE) == 106751
+
+    final String[] binaryUnits = {"B", "KiB", "MiB", "GiB", "TiB", "PiB"};
+    for (int i = 0; i < binaryUnits.length; ++i) {
+      BINARY_FORMATTERS.add(bin(10,    i, 1, "_" + binaryUnits[i]));
+      BINARY_FORMATTERS.add(bin(100,   i, 2, "_" + binaryUnits[i]));
+      BINARY_FORMATTERS.add(bin(1000,  i, 3, "_" + binaryUnits[i]));
+      BINARY_FORMATTERS.add(bin(10000, i, 4, "_" + binaryUnits[i]));
+    }
+    BINARY_FORMATTERS.add(new ValueFormatter(Long.MAX_VALUE, 4, "_PiB", v -> v >> 50));
+
+    final String[] decimalUnits = {"", "_k", "_M", "_G", "_T", "_P"};
+    for (int i = 0; i < decimalUnits.length; ++i) {
+      final int pow = i * 3;
+      DECIMAL_FORMATTERS.add(dec(10,   pow, 1, decimalUnits[i]));
+      DECIMAL_FORMATTERS.add(dec(100,  pow, 2, decimalUnits[i]));
+      DECIMAL_FORMATTERS.add(dec(1000, pow, 3, decimalUnits[i]));
+    }
+    DECIMAL_FORMATTERS.add(new ValueFormatter(Long.MAX_VALUE, 1, "_E", v -> v / pow10(1, 18)));
   }
 
   private static ValueFormatter fmt(long max, int width, String suffix, TimeUnit unit) {
-    return new ValueFormatter(max, width, suffix, unit);
+    return new ValueFormatter(max, width, suffix, v -> unit.convert(v, TimeUnit.NANOSECONDS));
+  }
+
+  private static ValueFormatter bin(long max, int pow, int width, String suffix) {
+    final int shift = pow * 10;
+    final long maxBytes = (shift == 0) ? max : max << shift;
+    return new ValueFormatter(maxBytes, width, suffix, v -> v >> shift);
+  }
+
+  private static ValueFormatter dec(long max, int pow, int width, String suffix) {
+    final long factor = pow10(1, pow);
+    final long maxBytes = max * factor;
+    return new ValueFormatter(maxBytes, width, suffix, v -> v / factor);
+  }
+
+  private static long pow10(long a, int b) {
+    long r = a;
+    for (int i = 0; i < b; ++i) {
+      r *= 10;
+    }
+    return r;
   }
 
   private BucketFunctions() {
   }
 
-  private static ValueFormatter getFormatter(long max) {
-    for (ValueFormatter f : FORMATTERS) {
+  private static ValueFormatter getFormatter(List<ValueFormatter> fmts, long max) {
+    for (ValueFormatter f : fmts) {
       if (max < f.max) {
         return f;
       }
     }
-    return new ValueFormatter(max, ("" + max).length(), "ns", TimeUnit.NANOSECONDS);
+    return fmts.get(fmts.size() - 1);
+  }
+
+  private static LongFunction<String> biasZero(
+      String ltZero, String gtMax, long max, ValueFormatter f) {
+    List<Bucket> buckets = new ArrayList<>();
+    buckets.add(new Bucket(ltZero, 0L));
+    buckets.add(f.newBucket(max / 8));
+    buckets.add(f.newBucket(max / 4));
+    buckets.add(f.newBucket(max / 2));
+    buckets.add(f.newBucket(max));
+    return new ListBucketFunction(buckets, gtMax);
   }
 
   private static LongFunction<String> timeBiasZero(
       String ltZero, String gtMax, long max, TimeUnit unit) {
-    final long nanos = unit.toNanos(max);
-    final ValueFormatter f = getFormatter(nanos);
-    final long v = f.unit().convert(max, unit);
+    final long v = unit.toNanos(max);
+    final ValueFormatter f = getFormatter(TIME_FORMATTERS, v);
+    return biasZero(ltZero, gtMax, v, f);
+  }
+
+  private static LongFunction<String> biasMax(
+      String ltZero, String gtMax, long max, ValueFormatter f) {
     List<Bucket> buckets = new ArrayList<>();
     buckets.add(new Bucket(ltZero, 0L));
-    buckets.add(f.newBucket(v / 8));
-    buckets.add(f.newBucket(v / 4));
-    buckets.add(f.newBucket(v / 2));
-    buckets.add(f.newBucket(v));
+    buckets.add(f.newBucket(max - max / 2));
+    buckets.add(f.newBucket(max - max / 4));
+    buckets.add(f.newBucket(max - max / 8));
+    buckets.add(f.newBucket(max));
     return new ListBucketFunction(buckets, gtMax);
   }
 
   private static LongFunction<String> timeBiasMax(
       String ltZero, String gtMax, long max, TimeUnit unit) {
-    final long nanos = unit.toNanos(max);
-    final ValueFormatter f = getFormatter(nanos);
-    final long v = f.unit().convert(max, unit);
-    List<Bucket> buckets = new ArrayList<>();
-    buckets.add(new Bucket(ltZero, 0L));
-    buckets.add(f.newBucket(v - v / 2));
-    buckets.add(f.newBucket(v - v / 4));
-    buckets.add(f.newBucket(v - v / 8));
-    buckets.add(f.newBucket(v));
-    return new ListBucketFunction(buckets, gtMax);
+    final long v = unit.toNanos(max);
+    final ValueFormatter f = getFormatter(TIME_FORMATTERS, v);
+    return biasMax(ltZero, gtMax, v, f);
   }
 
   /**
@@ -188,12 +247,47 @@ public final class BucketFunctions {
   }
 
   /**
+   * Returns a function that maps size values in bytes to a set of buckets. The buckets will
+   * use <a href="https://en.wikipedia.org/wiki/Binary_prefix">binary prefixes</a> representing
+   * powers of 1024. If you want powers of 1000, then see {@link #decimal(long)}.
+   *
+   * @param max
+   *     Maximum expected size of data being recorded. Values greater than this amount will be
+   *     mapped to a "large" bucket.
+   * @return
+   *     Function mapping size values to string labels. The labels for buckets will sort
+   *     so they can be used with a simple group by.
+   */
+  public static LongFunction<String> bytes(long max) {
+    ValueFormatter f = getFormatter(BINARY_FORMATTERS, max);
+    return biasZero("negative", "large", max, f);
+  }
+
+  /**
+   * Returns a function that maps size values to a set of buckets. The buckets will
+   * use <a href="https://en.wikipedia.org/wiki/Metric_prefix">decimal prefixes</a> representing
+   * powers of 1000. If you are measuring quantities in bytes and want powers of 1024, then see
+   * {@link #bytes(long)}.
+   *
+   * @param max
+   *     Maximum expected size of data being recorded. Values greater than this amount will be
+   *     mapped to a "large" bucket.
+   * @return
+   *     Function mapping size values to string labels. The labels for buckets will sort
+   *     so they can be used with a simple group by.
+   */
+  public static LongFunction<String> decimal(long max) {
+    ValueFormatter f = getFormatter(DECIMAL_FORMATTERS, max);
+    return biasZero("negative", "large", max, f);
+  }
+
+  /**
    * Format a value as a bucket label.
    */
   static class ValueFormatter {
     private final long max;
     private final String fmt;
-    private final TimeUnit unit;
+    private final LongUnaryOperator cnv;
 
     /**
      * Create a new instance.
@@ -204,13 +298,13 @@ public final class BucketFunctions {
      *     Number of digits to use for the numeric part of the label.
      * @param suffix
      *     Unit suffix appended to the label.
-     * @param unit
-     *     Unit for the value in the label.
+     * @param cnv
+     *     Value conversion function. Converts the input value to the right units for the label.
      */
-    ValueFormatter(long max, int width, String suffix, TimeUnit unit) {
+    ValueFormatter(long max, int width, String suffix, LongUnaryOperator cnv) {
       this.max = max;
       this.fmt = "%0" + width + "d" + suffix;
-      this.unit = unit;
+      this.cnv = cnv;
     }
 
     /** Return the max value intended for this formatter. */
@@ -218,20 +312,19 @@ public final class BucketFunctions {
       return max;
     }
 
-    /** Return the unit for the formatter. */
-    TimeUnit unit() {
-      return unit;
+    /** Apply conversion function to value. */
+    long convert(long v) {
+      return cnv.applyAsLong(v);
     }
 
     /** Convert the value {@code v} into a bucket label string. */
     String apply(long v) {
-      return String.format(fmt, unit.convert(v, TimeUnit.NANOSECONDS));
+      return String.format(fmt, cnv.applyAsLong(v));
     }
 
     /** Return a new bucket for the specified value. */
     Bucket newBucket(long v) {
-      final long nanos = unit.toNanos(v);
-      return new Bucket(apply(nanos), nanos);
+      return new Bucket(apply(v), v);
     }
   }
 
@@ -246,8 +339,8 @@ public final class BucketFunctions {
 
     @Override public String apply(long amount) {
       for (Bucket b : buckets) {
-        if (amount < b.upperBoundary) {
-          return b.name();
+        if (amount <= b.upperBoundary) {
+          return b.name;
         }
       }
       return fallback;
@@ -261,14 +354,6 @@ public final class BucketFunctions {
     Bucket(String name, long upperBoundary) {
       this.name = name;
       this.upperBoundary = upperBoundary;
-    }
-
-    String name() {
-      return name;
-    }
-
-    long upperBoundary() {
-      return upperBoundary;
     }
 
     @Override public String toString() {
