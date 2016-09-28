0056b0d889b7cd582e55634f43421d5328f1f138
brharrington <brharrington@gmail.com>
2016-09-28 05:49:24 -0700

    remove deprecated dynamic id classes (#320)
    Follow up for #317 to remove the deprecated calls
    and classes. There were not a lot of internal uses
    and all have received PRs to move off of the
    deprecated calls.

--- spectator-api/src/main/java/com/netflix/spectator/api/ExtendedRegistry.java
+++ spectator-api/src/main/java/com/netflix/spectator/api/ExtendedRegistry.java
@@ -53,23 +53,11 @@ public final class ExtendedRegistry implements Registry {
     return impl.createId(name, tags);
   }
 
-  @Override public DynamicId createDynamicId(String name) {
-    return impl.createDynamicId(name);
-  }
-
-  @Override public DynamicId createDynamicId(String name, Iterable<TagFactory> tagFactories) {
-    return impl.createDynamicId(name, tagFactories);
-  }
-
   @Override public void register(Meter meter) {
     impl.register(meter);
   }
 
   @Override public Counter counter(Id id) {
-    return impl.counter(id);
-  }
-
-  @Override public Counter counter(DynamicId id) {
     return impl.counter(id);
   }
 
