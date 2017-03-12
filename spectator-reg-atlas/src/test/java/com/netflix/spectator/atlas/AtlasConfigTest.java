Netflix/spectator

commit 9048e1d0e4a8a71b0365c086b01147b0db689c0b

add config options to set the allowed characters (#378)
Adds options to Atlas config interface for setting the
allowed characters of tag keys and to support overrides
for some tag values. The main use-case is allowing some
group names to include `^` and `~` as was recently agreed
internally.

The character set aspect has been moved to a utility
class `AsciiSet` with some performance tuning. In particular
the following changes were made:

1. The string is checked first and a buffer is only allocated
   if there are invalid characters. For the expected common
   case where there are no changes this gives a speed up of
   about 4-5x. In benchmarks it was about 15% slower for
   the case where changes are needed.
2. Switched from `StringBuilder` to using a `char[]` for the
   buffer. This is about 3x faster.
3. After 2, we have an array with the exact string so it can
   be used directly for the string to avoid an allocation and
   copy of the final string object. In the included benchmarks
   this was about 25% faster for the cases where changes were
   needed.

spectator-reg-atlas/src/main/java/com/netflix/spectator/atlas/AtlasConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2016 Netflix, Inc.
+ * Copyright 2014-2017 Netflix, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -125,6 +125,23 @@ public interface AtlasConfig extends RegistryConfig {
    * The default is an empty map.
    */
   default Map<String, String> commonTags() {
+    return Collections.emptyMap();
+  }
+
+  /**
+   * Returns a pattern indicating the valid characters for a tag key or value. The character
+   * set for tag values can be overridden for a particular tag key using
+   * {@link #validTagValueCharacters()}. The default is {@code -._A-Za-z0-9}.
+   */
+  default String validTagCharacters() {
+    return "-._A-Za-z0-9";
+  }
+
+  /**
+   * Returns a map from tag key to a pattern indicating the valid characters for the values
+   * of that key. The default is an empty map.
+   */
+  default Map<String, String> validTagValueCharacters() {
     return Collections.emptyMap();
   }
 }

codequality/findbugs-exclude.xml
@@ -30,4 +30,10 @@
       <Bug pattern="NM_SAME_SIMPLE_NAME_AS_INTERFACE"/>
     </And>
   </Match>
+  <Match>
+    <And>
+      <Class name="com.netflix.spectator.impl.AsciiSet"/>
+      <Bug pattern="REC_CATCH_EXCEPTION"/>
+    </And>
+  </Match>
 </FindBugsFilter>

spectator-api/src/jmh/java/com/netflix/spectator/perf/StringCreate.java
@@ -1,0 +1,115 @@
+/*
+ * Copyright 2014-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.perf;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Constructor;
+import java.util.UUID;
+
+/**
+ * Compares methods for creating a string based on an existing character array that we
+ * know will not get modified after the string is created.
+ *
+ * <pre>
+ * Benchmark                          Mode  Cnt          Score         Error  Units
+ * StringCreate.methodHandle         thrpt   10  176057226.160 ± 5069495.131  ops/s
+ * StringCreate.naive                thrpt   10   58188947.972 ± 1284066.194  ops/s
+ * StringCreate.reflection           thrpt   10   75278354.480 ± 9173452.172  ops/s
+ * </pre>
+ */
+@State(Scope.Thread)
+public class StringCreate {
+
+  private final String str = UUID.randomUUID().toString();
+  private final char[] arr = str.toCharArray();
+
+  private static final Constructor<String> STRING_CONSTRUCTOR;
+  private static final MethodHandle STRING_CONSTRUCTOR_HANDLE;
+  static {
+    Constructor<String> constructor;
+    MethodHandle handle;
+    try {
+      constructor = String.class.getDeclaredConstructor(char[].class, boolean.class);
+      constructor.setAccessible(true);
+
+      handle = MethodHandles.lookup().unreflectConstructor(constructor);
+    } catch (Exception e) {
+      constructor = null;
+      handle = null;
+    }
+    STRING_CONSTRUCTOR = constructor;
+    STRING_CONSTRUCTOR_HANDLE = handle;
+  }
+
+
+  /**
+   * Creates a new string without copying the buffer if possible. The String class has a
+   * package private constructor that allows the buffer to be shared.
+   */
+  private static String newStringReflection(char[] buf) {
+    if (STRING_CONSTRUCTOR != null) {
+      try {
+        return STRING_CONSTRUCTOR.newInstance(buf, true);
+      } catch (Exception e) {
+        return new String(buf);
+      }
+    } else {
+      return new String(buf);
+    }
+  }
+
+  /**
+   * Creates a new string without copying the buffer if possible. The String class has a
+   * package private constructor that allows the buffer to be shared.
+   */
+  private static String newStringMethodHandle(char[] buf) {
+    if (STRING_CONSTRUCTOR_HANDLE != null) {
+      try {
+        return (String) STRING_CONSTRUCTOR_HANDLE.invokeExact(buf, true);
+      } catch (Throwable t) {
+        return new String(buf);
+      }
+    } else {
+      return new String(buf);
+    }
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void naive(Blackhole bh) {
+    bh.consume(new String(arr));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void reflection(Blackhole bh) {
+    bh.consume(newStringReflection(arr));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void methodHandle(Blackhole bh) {
+    bh.consume(newStringMethodHandle(arr));
+  }
+
+}

spectator-api/src/jmh/java/com/netflix/spectator/perf/StringForeach.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright 2014-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.perf;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.OptionalInt;
+import java.util.UUID;
+
+/**
+ * Sanity check on iterating over a string using newer language features. Streams are
+ * really slow and should be avoided for the time being.
+ *
+ * <pre>
+ * Benchmark                           Mode  Cnt         Score         Error  Units
+ * StringForeach.arrayForLoop         thrpt   10  69226595.071 ± 3958789.722  ops/s
+ * StringForeach.arrayForeach         thrpt   10  69282507.672 ± 2090321.612  ops/s
+ * StringForeach.strForLoop           thrpt   10  55900971.604 ± 2190481.894  ops/s
+ * StringForeach.stream               thrpt   10   4337780.512 ±  102242.727  ops/s
+ * </pre>
+ */
+@State(Scope.Thread)
+public class StringForeach {
+
+  private final String str = UUID.randomUUID().toString();
+  private final char[] arr = str.toCharArray();
+
+  @Threads(1)
+  @Benchmark
+  public void arrayForLoop(Blackhole bh) {
+    int v = 0;
+    int n = arr.length;
+    for (int i = 0; i < n; ++i) {
+      v += arr[i];
+    }
+    bh.consume(v);
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void arrayForeach(Blackhole bh) {
+    int v = 0;
+    for (char c : arr) {
+      v += c;
+    }
+    bh.consume(v);
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void strForLoop(Blackhole bh) {
+    int v = 0;
+    int n = str.length();
+    for (int i = 0; i < n; ++i) {
+      v += str.charAt(i);
+    }
+    bh.consume(v);
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void stream(Blackhole bh) {
+    OptionalInt v = str.chars().reduce((acc, c) -> acc + c);
+    bh.consume(v.isPresent() ? v.getAsInt() : 0);
+  }
+
+}

spectator-api/src/jmh/java/com/netflix/spectator/perf/StringReplace.java
@@ -1,0 +1,164 @@
+/*
+ * Copyright 2014-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.perf;
+
+import com.netflix.spectator.impl.AsciiSet;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.UUID;
+
+/**
+ * Compares a number of approaches for quickly replacing invalid characters that are part
+ * of a string. The goal is to be fast and preferably minimize the number of allocations.
+ *
+ * The {@link AsciiSet} class uses a combination of array approach, check first, and method
+ * handle to avoid an additional allocation and array copy when creating the string (see
+ * {@link StringCreate} benchmark for more details).
+ *
+ * <pre>
+ * Benchmark                                Mode  Cnt         Score         Error  Units
+ *
+ * StringReplace.bad_array                 thrpt   10  16714173.859 ±  234209.339  ops/s
+ * StringReplace.bad_asciiSet              thrpt   10  18107063.738 ±  360271.524  ops/s
+ * StringReplace.bad_checkFirst            thrpt   10  14165041.869 ±  208433.352  ops/s
+ * StringReplace.bad_naive                 thrpt   10    530188.254 ±    8342.491  ops/s
+ * StringReplace.bad_stringBuilder         thrpt   10   5754313.189 ±  138045.465  ops/s
+ *
+ * StringReplace.ok_array                  thrpt   10  18899940.964 ±  293219.495  ops/s
+ * StringReplace.ok_asciiSet               thrpt   10  30230490.618 ± 1096911.677  ops/s
+ * StringReplace.ok_checkFirst             thrpt   10  30601837.288 ±  453161.602  ops/s
+ * StringReplace.ok_naive                  thrpt   10    491380.343 ±    9738.662  ops/s
+ * StringReplace.ok_stringBuilder          thrpt   10   6673496.163 ±  115877.036  ops/s
+ * </pre>
+ */
+@State(Scope.Thread)
+public class StringReplace {
+
+  private final AsciiSet set = AsciiSet.fromPattern("-._A-Za-z0-9");
+  private final String ok = UUID.randomUUID().toString();
+  private final String bad = ok.replace('-', ' ');
+
+  private void replace(StringBuilder buf, String input, char replacement) {
+    final int n = input.length();
+    for (int i = 0; i < n; ++i) {
+      final char c = input.charAt(i);
+      if (set.contains(c)) {
+        buf.append(c);
+      } else {
+        buf.append(replacement);
+      }
+    }
+  }
+
+  private void replace(char[] buf, String input, char replacement) {
+    final int n = input.length();
+    for (int i = 0; i < n; ++i) {
+      final char c = input.charAt(i);
+      if (!set.contains(c)) {
+        buf[i] = replacement;
+      }
+    }
+  }
+
+  private String naive(String input, char replacement) {
+    return input.replaceAll("[-._A-Za-z0-9]", "" + replacement);
+  }
+
+  private String stringBuilder(String input, char replacement) {
+    StringBuilder buf = new StringBuilder(input.length());
+    replace(buf, input, replacement);
+    return buf.toString();
+  }
+
+  private String array(String input, char replacement) {
+    char[] buf = input.toCharArray();
+    replace(buf, input, replacement);
+    return new String(buf);
+  }
+
+  private String checkFirst(String input, char replacement) {
+    return set.containsAll(input) ? input : array(input, replacement);
+  }
+
+  private String asciiSet(String input, char replacement) {
+    return set.replaceNonMembers(input, replacement);
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void ok_naive(Blackhole bh) {
+    bh.consume(naive(ok, '_'));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void ok_stringBuilder(Blackhole bh) {
+    bh.consume(stringBuilder(ok, '_'));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void ok_array(Blackhole bh) {
+    bh.consume(array(ok, '_'));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void ok_checkFirst(Blackhole bh) {
+    bh.consume(checkFirst(ok, '_'));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void ok_asciiSet(Blackhole bh) {
+    bh.consume(asciiSet(ok, '_'));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void bad_naive(Blackhole bh) {
+    bh.consume(naive(bad, '_'));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void bad_stringBuilder(Blackhole bh) {
+    bh.consume(stringBuilder(bad, '_'));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void bad_array(Blackhole bh) {
+    bh.consume(array(bad, '_'));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void bad_checkFirst(Blackhole bh) {
+    bh.consume(checkFirst(bad, '_'));
+  }
+
+  @Threads(1)
+  @Benchmark
+  public void bad_asciiSet(Blackhole bh) {
+    bh.consume(asciiSet(bad, '_'));
+  }
+
+}

spectator-api/src/main/java/com/netflix/spectator/impl/AsciiSet.java
@@ -1,0 +1,197 @@
+/*
+ * Copyright 2014-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.impl;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Constructor;
+
+/**
+ * Utility class for quickly checking if a string contains only characters contained within
+ * the given set. The set is limited to basic ascii with ranges, if you need more advanced
+ * patterns then regular expressions are a better option though it will likely come with a
+ * steep performance penalty.
+ *
+ * <p><b>This class is an internal implementation detail only intended for use within spectator.
+ * It is subject to change without notice.</b></p>
+ */
+public final class AsciiSet {
+
+  private static final MethodHandle STRING_CONSTRUCTOR;
+  static {
+    MethodHandle handle;
+    try {
+      Constructor<String> ctor = String.class.getDeclaredConstructor(char[].class, boolean.class);
+      ctor.setAccessible(true);
+      handle = MethodHandles.lookup().unreflectConstructor(ctor);
+    } catch (Exception e) {
+      handle = null;
+    }
+    STRING_CONSTRUCTOR = handle;
+  }
+
+  /**
+   * Creates a new string without copying the buffer if possible. The String class has a
+   * package private constructor that allows the buffer to be shared.
+   */
+  @SuppressWarnings("PMD.AvoidCatchingThrowable")
+  private static String newString(char[] buf) {
+    if (STRING_CONSTRUCTOR != null) {
+      try {
+        return (String) STRING_CONSTRUCTOR.invokeExact(buf, true);
+      } catch (Throwable t) {
+        // Note: `invokeExact` explicitly throws Throwable to propagate any exception of the
+        // method unchanged. For our purposes we just fallback to the string constructor.
+        return new String(buf);
+      }
+    } else {
+      return new String(buf);
+    }
+  }
+
+  /**
+   * Create a set containing ascii characters using a simple pattern. The pattern is similar
+   * to a character set in regex. For example, {@code ABC} would contain the characters
+   * {@code A}, {@code B}, and {@code C}. Ranges are supported so all uppercase letters could
+   * be specified as {@code A-Z}. The dash, {@code -}, will be included as part of the set if
+   * it is at the start or end of the pattern.
+   *
+   * @param pattern
+   *     String specification of the character set.
+   * @return
+   *     Set containing the characters specified in {@code pattern}.
+   */
+  public static AsciiSet fromPattern(String pattern) {
+    final boolean[] members = new boolean[128];
+    final int n = pattern.length();
+    for (int i = 0; i < n; ++i) {
+      final char c = pattern.charAt(i);
+      if (c >= members.length) {
+        throw new IllegalArgumentException("invalid pattern, '" + c + "' is not ascii");
+      }
+
+      final boolean isStartOrEnd = i == 0 || i == n - 1;
+      if (isStartOrEnd || c != '-') {
+        members[c] = true;
+      } else {
+        final char s = pattern.charAt(i - 1);
+        final char e = pattern.charAt(i + 1);
+        for (char v = s; v <= e; ++v) {
+          members[v] = true;
+        }
+      }
+    }
+    return new AsciiSet(members);
+  }
+
+  /**
+   * Converts the members array to a pattern string. Used to provide a user friendly toString
+   * implementation for the set.
+   */
+  private static String toPattern(boolean[] members) {
+    StringBuilder buf = new StringBuilder();
+    boolean previous = false;
+    char s = 0;
+    for (int i = 0; i < members.length; ++i) {
+      if (members[i] && !previous) {
+        s = (char) i;
+      } else if (!members[i] && previous) {
+        final char e = (char) (i - 1);
+        append(buf, s, e);
+      }
+      previous = members[i];
+    }
+    if (previous) {
+      final char e = (char) (members.length - 1);
+      append(buf, s, e);
+    }
+    return buf.toString();
+  }
+
+  private static void append(StringBuilder buf, char s, char e) {
+    switch (e - s) {
+      case 0:  buf.append(s);                       break;
+      case 1:  buf.append(s).append(e);             break;
+      default: buf.append(s).append('-').append(e); break;
+    }
+  }
+
+  private final String pattern;
+  private final boolean[] members;
+
+  private AsciiSet(boolean[] members) {
+    this.members = Preconditions.checkNotNull(members, "members array cannot be null");
+    this.pattern = toPattern(members);
+  }
+
+  /**
+   * Returns true if the character contained within the set. This is a constant time
+   * operation.
+   */
+  public boolean contains(char c) {
+    return c < 128 && members[c];
+  }
+
+  /**
+   * Returns true if all characters in the string are contained within the set.
+   */
+  public boolean containsAll(CharSequence str) {
+    final int n = str.length();
+    for (int i = 0; i < n; ++i) {
+      if (!contains(str.charAt(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Replace all characters in the input string with the replacement character.
+   */
+  public String replaceNonMembers(String input, char replacement) {
+    if (!contains(replacement)) {
+      throw new IllegalArgumentException(replacement + " is not a member of " + pattern);
+    }
+    return containsAll(input) ? input : replaceNonMembersImpl(input, replacement);
+  }
+
+  private String replaceNonMembersImpl(String input, char replacement) {
+    final int n = input.length();
+    final char[] buf = input.toCharArray();
+    for (int i = 0; i < n; ++i) {
+      final char c = buf[i];
+      if (!contains(c)) {
+        buf[i] = replacement;
+      }
+    }
+    return newString(buf);
+  }
+
+  @Override public String toString() {
+    return pattern;
+  }
+
+  @Override public int hashCode() {
+    return pattern.hashCode();
+  }
+
+  @Override public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || !(obj instanceof AsciiSet)) return false;
+    AsciiSet other = (AsciiSet) obj;
+    return pattern.equals(other.pattern);
+  }
+}

commit 9a3263af308053c86e7a9db895791647f14986a2

update pmd to 5.5.4
This is mostly to work around a bug in 5.3.3:

https://sourceforge.net/p/pmd/bugs/1384/

This fixes the following errors in build logs that started
showing up after #378:

```
Exception applying rule ConsecutiveLiteralAppends on file
spectator/spectator-api/src/main/java/com/netflix/spectator/impl/AsciiSet.java,
continuing with next rule

java.lang.NullPointerException
```

build.gradle
@@ -119,7 +119,7 @@ subprojects {
   }
 
   pmd {
-    toolVersion = '5.3.3'
+    toolVersion = '5.5.4'
     ignoreFailures = false
     sourceSets = [sourceSets.main]
     ruleSets = []

codequality/pmd.xml
@@ -10,6 +10,7 @@
 
   <rule ref="rulesets/internal/all-java.xml">
     <exclude name="AbstractNaming"/>
+    <exclude name="AccessorMethodGeneration"/>
     <exclude name="AddEmptyString"/>
     <exclude name="AppendCharacterWithChar"/>
     <exclude name="ArrayIsStoredDirectly"/>
@@ -27,7 +28,7 @@
     <exclude name="AvoidUsingShortType"/>
     <exclude name="AvoidUsingVolatile"/>
     <exclude name="BeanMembersShouldSerialize"/>
-    <exclude name="BooleanInversion"/>
+    <exclude name="CommentDefaultAccessModifier"/>
     <exclude name="CommentRequired"/>
     <exclude name="CommentSize"/>
     <exclude name="CompareObjectsWithEquals"/>
@@ -36,6 +37,7 @@
     <exclude name="DataflowAnomalyAnalysis"/>
     <exclude name="DefaultPackage"/>
     <exclude name="DoNotUseThreads"/>
+    <exclude name="DontImportJavaLang"/> <!-- Incorrectly flags java.lang.invoke.* -->
     <exclude name="DuplicateImports"/>
     <exclude name="EmptyMethodInAbstractClassShouldBeAbstract"/>
     <exclude name="ExcessiveImports"/>

spectator-api/src/main/java/com/netflix/spectator/api/FilteredIterator.java
@@ -56,10 +56,13 @@ class FilteredIterator<T> implements Iterator<T> {
     return item != null;
   }
 
+  @SuppressWarnings("PMD.UnnecessaryLocalBeforeReturn")
   @Override public T next() throws NoSuchElementException {
     if (item == null) {
       throw new NoSuchElementException("next() called after reaching end of iterator");
     }
+    // Note: PMD flags this, but the local tmp is necessary because findNext will change
+    // the value of item
     T tmp = item;
     findNext();
     return tmp;

spectator-api/src/main/java/com/netflix/spectator/api/RegistryConfig.java
@@ -35,7 +35,7 @@ public interface RegistryConfig {
   /** Should an exception be thrown for warnings? */
   default boolean propagateWarnings() {
     String v = get("propagateWarnings");
-    return (v == null) ? false : Boolean.valueOf(v);
+    return v != null && Boolean.valueOf(v);
   }
 
   /**

spectator-api/src/main/java/com/netflix/spectator/impl/Config.java
@@ -123,7 +123,7 @@ public final class Config {
   public static <T> T createProxy(Class<T> cls, Function<String, String> props) {
     final Constructor<MethodHandles.Lookup> constructor = getConstructor();
     final Class<?>[] interfaces = new Class<?>[] {cls};
-    final Object obj = Proxy.newProxyInstance(classLoader(), interfaces, (proxy, method, args) -> {
+    return (T) Proxy.newProxyInstance(classLoader(), interfaces, (proxy, method, args) -> {
       final String name = method.getName();
       if (method.isDefault()) {
         final Class<?> declaringClass = method.getDeclaringClass();
@@ -142,7 +142,6 @@ public final class Config {
         return valueOf(rt, v);
       }
     });
-    return (T) obj;
   }
 
   /**

spectator-reg-atlas/src/main/java/com/netflix/spectator/atlas/AtlasConfig.java
@@ -39,7 +39,7 @@ public interface AtlasConfig extends RegistryConfig {
    */
   default boolean enabled() {
     String v = get("atlas.enabled");
-    return (v == null) ? true : Boolean.valueOf(v);
+    return v == null || Boolean.valueOf(v);
   }
 
   /**
@@ -65,7 +65,7 @@ public interface AtlasConfig extends RegistryConfig {
    */
   default boolean lwcEnabled() {
     String v = get("atlas.lwc.enabled");
-    return (v == null) ? false : Boolean.valueOf(v);
+    return v != null && Boolean.valueOf(v);
   }
 
   /** Returns the frequency for refreshing config settings from the LWC service. */

spectator-reg-atlas/src/test/java/com/netflix/spectator/atlas/AtlasConfigTest.java
@@ -1,0 +1,92 @@
+/*
+ * Copyright 2014-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.atlas;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RunWith(JUnit4.class)
+public class AtlasConfigTest {
+
+  @Test
+  public void enabledByDefault() {
+    Map<String, String> props = Collections.emptyMap();
+    AtlasConfig config = props::get;
+    Assert.assertTrue(config.enabled());
+  }
+
+  @Test
+  public void explicitlyEnabled() {
+    Map<String, String> props = new HashMap<>();
+    props.put("atlas.enabled", "true");
+    AtlasConfig config = props::get;
+    Assert.assertTrue(config.enabled());
+  }
+
+  @Test
+  public void explicitlyDisabled() {
+    Map<String, String> props = new HashMap<>();
+    props.put("atlas.enabled", "false");
+    AtlasConfig config = props::get;
+    Assert.assertFalse(config.enabled());
+  }
+
+  @Test
+  public void enabledBadValue() {
+    Map<String, String> props = new HashMap<>();
+    props.put("atlas.enabled", "abc");
+    AtlasConfig config = props::get;
+    Assert.assertFalse(config.enabled());
+  }
+
+  @Test
+  public void lwcDisabledByDefault() {
+    Map<String, String> props = Collections.emptyMap();
+    AtlasConfig config = props::get;
+    Assert.assertFalse(config.lwcEnabled());
+  }
+
+  @Test
+  public void lwcExplicitlyEnabled() {
+    Map<String, String> props = new HashMap<>();
+    props.put("atlas.lwc.enabled", "true");
+    AtlasConfig config = props::get;
+    Assert.assertTrue(config.lwcEnabled());
+  }
+
+  @Test
+  public void lwcExplicitlyDisabled() {
+    Map<String, String> props = new HashMap<>();
+    props.put("atlas.lwc.enabled", "false");
+    AtlasConfig config = props::get;
+    Assert.assertFalse(config.lwcEnabled());
+  }
+
+  @Test
+  public void lwcEnabledBadValue() {
+    Map<String, String> props = new HashMap<>();
+    props.put("atlas.lwc.enabled", "abc");
+    AtlasConfig config = props::get;
+    Assert.assertFalse(config.lwcEnabled());
+  }
+}