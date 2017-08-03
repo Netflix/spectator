8e327635757dd5432e27871ad58e58f2a8c1eba2
Brian Harrington <brharrington@gmail.com>
2017-08-03 09:08:54 -0700

    add appender for log4j 1.x
    Some internal users still run using log4j 1.x and
    have some difficulty moving forward. This provides
    an appender to collect logging stats similar to the
    existing log4j2 extension.
    
    Fixes #422

---
+++ docs/ext/log4j1.md
@@ -0,0 +1,50 @@
+# Log4j1 Appender
+
+Custom appender for [log4j1](http://logging.apache.org/log4j/1.2/) to track the number of
+log messages reported. 
+
+!!! note
+    Log4j 1.x has reached [end of life][eol] and is no longer supported by Apache. This extension
+    is provided for some users that have difficulty moving to a supported version of log4j.
+    
+[eol]: https://blogs.apache.org/foundation/entry/apache_logging_services_project_announces
+
+## Getting Started
+
+To use it simply add a dependency:
+
+```
+com.netflix.spectator:spectator-ext-log4j1:0.56.0
+```
+
+Then in your log4j configuration specify the `com.netflix.spectator.log4j.SpectatorAppender`.
+In a properties file it would look something like:
+
+```
+log4j.rootLogger=ALL, A1
+log4j.appender.A1=com.netflix.spectator.log4j.SpectatorAppender
+```
+
+## Metrics
+
+### log4j.numMessages
+
+Counters showing the number of messages that have been passed to the appender.
+
+**Unit:** messages/second
+
+**Dimensions:**
+
+* `loglevel`: standard log level of the events.
+
+### log4j.numStackTraces
+
+Counter for the number of messages with stack traces written to the logs.
+
+**Unit:** messages/second
+
+**Dimensions:**
+
+* `loglevel`: standard log level of the events.
+* `exception`: simple class name for the exception that was thrown.
+* `file`: file name for where the exception was thrown.
---
+++ spectator-ext-log4j1/src/main/java/com/netflix/spectator/log4j/SpectatorAppender.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2014-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.log4j;
+
+import com.netflix.spectator.api.Id;
+import com.netflix.spectator.api.Registry;
+import com.netflix.spectator.api.Spectator;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LocationInfo;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
+
+/**
+ * Appender that tracks the number of messages that pass through.
+ */
+public final class SpectatorAppender extends AppenderSkeleton {
+
+  private final Registry registry;
+  private final Id[] numMessages;
+  private final Id[] numStackTraces;
+
+  /** Create a new instance of the appender. */
+  public SpectatorAppender() {
+    this(Spectator.globalRegistry());
+  }
+
+  /** Create a new instance of the appender. */
+  public SpectatorAppender(Registry registry) {
+    this.registry = registry;
+
+    final LevelTag[] levels = LevelTag.values();
+    numMessages = new Id[levels.length];
+    numStackTraces = new Id[levels.length];
+    for (int i = 0; i < levels.length; ++i) {
+      String appender = getName() == null ? "unknown" : getName();
+      numMessages[i] = registry.createId("log4j.numMessages")
+          .withTag(levels[i]);
+      numStackTraces[i] = registry.createId("log4j.numStackTraces")
+          .withTag(levels[i]);
+    }
+  }
+
+  @Override protected void append(LoggingEvent event) {
+    final LevelTag level = LevelTag.get(event.getLevel());
+    registry.counter(numMessages[level.ordinal()]).increment();
+
+    ThrowableInformation info = event.getThrowableInformation();
+    if (info != null) {
+      LocationInfo loc = event.getLocationInformation();
+      final String file = (loc == null) ? "unknown" : loc.getFileName();
+      Id stackTraceId = numStackTraces[level.ordinal()]
+          .withTag("exception", info.getThrowable().getClass().getSimpleName())
+          .withTag("file", file);
+      registry.counter(stackTraceId).increment();
+    }
+  }
+
+  @Override public void close() {
+  }
+
+  @Override public boolean requiresLayout() {
+    return false;
+  }
+}
