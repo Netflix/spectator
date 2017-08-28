1639c2d82116b271fab141d6182785fa7f84cda6
Matt Zimmer <zimmermatt@gmail.com>
2017-08-28 13:11:45 -0700

    Issue #419, add thread pool monitor (#463)
    Added polled monitoring of common thread pool properties.  Where
    possible, Tomcat thread pool and executor metric names were used for
    consistency.

---
+++ spectator-api/src/main/java/com/netflix/spectator/api/patterns/ThreadPoolMonitor.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2014-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.api.patterns;
+
+import com.netflix.spectator.api.BasicTag;
+import com.netflix.spectator.api.Registry;
+import com.netflix.spectator.api.Tag;
+import com.netflix.spectator.impl.Preconditions;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Monitors and reports a common set of metrics for thread pools.
+ *
+ * <p>The following is the list of metrics reported followed by which {@link ThreadPoolExecutor}
+ * method populates it.</p>
+ *
+ * <pre>
+ *  +-----------------------------------------------------------------------------+
+ *  | Metric Name                  | Data Source                                  |
+ *  +------------------------------+----------------------------------------------+
+ *  |threadpool.taskCount          | {@link ThreadPoolExecutor#getTaskCount()}            |
+ *  +------------------------------+----------------------------------------------+
+ *  |threadpool.completedTaskCount | {@link ThreadPoolExecutor#getCompletedTaskCount()}   |
+ *  +------------------------------+----------------------------------------------+
+ *  |threadpool.currentThreadsBusy | {@link ThreadPoolExecutor#getActiveCount()}          |
+ *  +------------------------------+----------------------------------------------+
+ *  |threadpool.maxThreads         | {@link ThreadPoolExecutor#getMaximumPoolSize()}      |
+ *  +------------------------------+----------------------------------------------+
+ *  |threadpool.poolSize           | {@link ThreadPoolExecutor#getPoolSize()}             |
+ *  +------------------------------+----------------------------------------------+
+ *  |threadpool.corePoolSize       | {@link ThreadPoolExecutor#getCorePoolSize()}         |
+ *  +------------------------------+----------------------------------------------+
+ *  |threadpool.queueSize          | {@link ThreadPoolExecutor#getQueue()}.size()         |
+ *  +-----------------------------------------------------------------------------+
+ * </pre>
+ */
+public final class ThreadPoolMonitor {
+
+  /**
+   * The default ID tag name.
+   */
+  static final String ID_TAG_NAME = "id";
+
+  /**
+   * The default ID value.
+   */
+  static final String DEFAULT_ID = "default";
+
+  /**
+   * Task count meter name.
+   */
+  static final String TASK_COUNT = "threadpool.taskCount";
+
+  /**
+   * Completed task count meter name.
+   */
+  static final String COMPLETED_TASK_COUNT = "threadpool.completedTaskCount";
+
+  /**
+   * Current threads busy meter name.
+   */
+  static final String CURRENT_THREADS_BUSY = "threadpool.currentThreadsBusy";
+
+  /**
+   * Max threads meter name.
+   */
+  static final String MAX_THREADS = "threadpool.maxThreads";
+
+  /**
+   * Pool size meter name.
+   */
+  static final String POOL_SIZE = "threadpool.poolSize";
+
+  /**
+   * Core pool size meter name.
+   */
+  static final String CORE_POOL_SIZE = "threadpool.corePoolSize";
+
+  /**
+   * Queue size meter name.
+   */
+  static final String QUEUE_SIZE = "threadpool.queueSize";
+
+  // prevent direct instantiation.
+  private ThreadPoolMonitor() { }
+
+  /**
+   * Register the provided thread pool, optionally tagged with a name.
+   *
+   * @param registry the registry to use
+   * @param threadPool the thread pool on which to attach monitoring
+   * @param threadPoolName a name with which to tag the metrics (default name used if {@code null} or empty)
+   */
+  public static void attach(
+      final Registry registry,
+      final ThreadPoolExecutor threadPool,
+      final String threadPoolName) {
+
+    Preconditions.checkNotNull(registry, "registry");
+    Preconditions.checkNotNull(threadPool, "threadPool");
+
+    final String idValue;
+    if (threadPoolName == null || threadPoolName.isEmpty()) {
+      idValue = DEFAULT_ID;
+    } else {
+      idValue = threadPoolName;
+    }
+
+    final Tag idTag = new BasicTag(ID_TAG_NAME, idValue);
+
+    PolledMeter.using(registry)
+        .withName(TASK_COUNT)
+        .withTag(idTag)
+        .monitorMonotonicCounter(threadPool, ThreadPoolExecutor::getTaskCount);
+    PolledMeter.using(registry)
+        .withName(COMPLETED_TASK_COUNT)
+        .withTag(idTag)
+        .monitorMonotonicCounter(threadPool, ThreadPoolExecutor::getCompletedTaskCount);
+    PolledMeter.using(registry)
+        .withName(CURRENT_THREADS_BUSY)
+        .withTag(idTag)
+        .monitorValue(threadPool, ThreadPoolExecutor::getActiveCount);
+    PolledMeter.using(registry)
+        .withName(MAX_THREADS)
+        .withTag(idTag)
+        .monitorValue(threadPool, ThreadPoolExecutor::getMaximumPoolSize);
+    PolledMeter.using(registry)
+        .withName(POOL_SIZE)
+        .withTag(idTag)
+        .monitorValue(threadPool, ThreadPoolExecutor::getPoolSize);
+    PolledMeter.using(registry)
+        .withName(CORE_POOL_SIZE)
+        .withTag(idTag)
+        .monitorValue(threadPool, ThreadPoolExecutor::getCorePoolSize);
+    PolledMeter.using(registry)
+        .withName(QUEUE_SIZE)
+        .withTag(idTag)
+        .monitorValue(threadPool, tp -> tp.getQueue().size());
+  }
+}
---
+++ spectator-api/src/test/java/com/netflix/spectator/api/patterns/ThreadPoolMonitorTest.java
@@ -0,0 +1,404 @@
+/*
+ * Copyright 2014-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.api.patterns;
+
+import com.netflix.spectator.api.Counter;
+import com.netflix.spectator.api.DefaultRegistry;
+import com.netflix.spectator.api.Gauge;
+import com.netflix.spectator.api.Id;
+import com.netflix.spectator.api.Measurement;
+import com.netflix.spectator.api.Meter;
+import com.netflix.spectator.api.Registry;
+import com.netflix.spectator.api.Tag;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnit4.class)
+public class ThreadPoolMonitorTest {
+
+  private static final String THREAD_POOL_NAME = ThreadPoolMonitorTest.class.getSimpleName();
+
+  private Registry registry;
+  private LatchedThreadPoolExecutor latchedExecutor;
+
+  private static class TestRunnable implements Runnable {
+    private final CountDownLatch synchronizer;
+    private final CountDownLatch terminator;
+
+    TestRunnable(final CountDownLatch synchronizer, final CountDownLatch terminator) {
+      this.synchronizer = synchronizer;
+      this.terminator = terminator;
+    }
+
+    @Override
+    public void run() {
+      try {
+        synchronizer.countDown();
+        terminator.await(6, TimeUnit.SECONDS);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
+
+  private static class LatchedThreadPoolExecutor extends ThreadPoolExecutor {
+
+    private final AtomicInteger queueSize = new AtomicInteger();
+    private final AtomicInteger active = new AtomicInteger();
+    private final AtomicLong tasks = new AtomicLong();
+    private final AtomicLong completedTasks = new AtomicLong();
+    private volatile CountDownLatch completed;
+
+    LatchedThreadPoolExecutor(final CountDownLatch completed) {
+      super(3, 10, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+      this.completed = completed;
+    }
+
+    CountDownLatch getCompletedLatch() {
+      return this.completed;
+    }
+
+    void setCompletedLatch(final CountDownLatch completedLatch) {
+      this.completed = completedLatch;
+    }
+
+    @Override
+    public void execute(Runnable task) {
+      queueSize.incrementAndGet();
+      super.execute(task);
+    }
+
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+      queueSize.decrementAndGet();
+      active.incrementAndGet();
+      tasks.incrementAndGet();
+      super.beforeExecute(t, r);
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+      active.decrementAndGet();
+      completedTasks.incrementAndGet();
+      completed.countDown();
+      super.afterExecute(r, t);
+    }
+
+    @Override
+    public int getActiveCount() {
+      return active.get();
+    }
+
+    @Override
+    public long getTaskCount() {
+      return tasks.get();
+    }
+
+    @Override
+    public long getCompletedTaskCount() {
+      return completedTasks.get();
+    }
+
+    @Override
+    public BlockingQueue<Runnable> getQueue() {
+      return new LinkedBlockingQueue<Runnable>() {
+        @Override
+        public int size() {
+          return queueSize.get();
+        }
+      };
+    }
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    registry = new DefaultRegistry();
+    latchedExecutor = new LatchedThreadPoolExecutor(new CountDownLatch(1));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    registry = null;
+    latchedExecutor.shutdown();
+    latchedExecutor = null;
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void monitorThrowsIfNullRegistry() throws Exception {
+    ThreadPoolMonitor.attach(null, latchedExecutor, THREAD_POOL_NAME);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void monitorThrowsIfNullThreadPool() throws Exception {
+    ThreadPoolMonitor.attach(registry, null, THREAD_POOL_NAME);
+  }
+
+  @Test
+  public void monitorAcceptsNullThreadPoolName() {
+    ThreadPoolMonitor.attach(registry, latchedExecutor, null);
+  }
+
+  private Meter getMeter(String meterName, String threadPoolName) {
+    ThreadPoolMonitor.attach(registry, latchedExecutor, threadPoolName);
+    PolledMeter.update(registry);
+    final Id id = registry.createId(meterName).withTag(ThreadPoolMonitor.ID_TAG_NAME,
+        (threadPoolName == null || threadPoolName.isEmpty()) ? ThreadPoolMonitor.DEFAULT_ID : threadPoolName);
+    return registry.get(id);
+  }
+
+  private Meter getMeter(String meterName) {
+    return getMeter(meterName, THREAD_POOL_NAME);
+  }
+
+  private Gauge getGauge(String meterName) {
+    return (Gauge) getMeter(meterName, THREAD_POOL_NAME);
+  }
+
+  private Counter getCounter(String meterName) {
+    return (Counter) getMeter(meterName, THREAD_POOL_NAME);
+  }
+
+  @Test
+  public void metricsAreTaggedWithProvidedThreadPoolName() {
+    checkIdTagValue(getMeter(ThreadPoolMonitor.MAX_THREADS), getClass().getSimpleName());
+  }
+
+  @Test
+  public void metricsAreTaggedWithDefaultThreadPoolNameIfNull() {
+    checkIdTagValue(getMeter(ThreadPoolMonitor.MAX_THREADS, null), ThreadPoolMonitor.DEFAULT_ID);
+  }
+
+  @Test
+  public void metricsAreTaggedWithDefaultThreadPoolNameIfEmpty() {
+    checkIdTagValue(getMeter(ThreadPoolMonitor.MAX_THREADS, ""), ThreadPoolMonitor.DEFAULT_ID);
+  }
+
+  private void checkIdTagValue(Meter meter, String expectedIdValue) {
+    final Iterable<Measurement> measurements = meter.measure();
+    final Iterator<Measurement> measurementIterator = measurements.iterator();
+    assertTrue(measurementIterator.hasNext());
+
+    final Iterator<Tag> tags = measurementIterator.next().id().tags().iterator();
+    assertTrue(tags.hasNext());
+    Tag tag = tags.next();
+    assertEquals(ThreadPoolMonitor.ID_TAG_NAME, tag.key());
+    assertEquals(expectedIdValue, tag.value());
+  }
+
+  @Test
+  public void threadPoolMonitorHasTaskCountMeter() {
+    assertNotNull(getMeter(ThreadPoolMonitor.TASK_COUNT));
+  }
+
+  @Test
+  public void threadPoolMonitorHasCompletedTaskCountMeter() {
+    assertNotNull(getMeter(ThreadPoolMonitor.COMPLETED_TASK_COUNT));
+  }
+
+  @Test
+  public void threadPoolMonitorHasCurrentThreadsBusyMeter() {
+    assertNotNull(getMeter(ThreadPoolMonitor.CURRENT_THREADS_BUSY));
+  }
+
+  @Test
+  public void threadPoolMonitorHasMaxThreadsMeter() {
+    assertNotNull(getMeter(ThreadPoolMonitor.MAX_THREADS));
+  }
+
+  @Test
+  public void threadPoolMonitorHasPoolSizeMeter() {
+    assertNotNull(getMeter(ThreadPoolMonitor.POOL_SIZE));
+  }
+
+  @Test
+  public void threadPoolMonitorHasCorePoolSizeMeter() {
+    assertNotNull(getMeter(ThreadPoolMonitor.CORE_POOL_SIZE));
+  }
+
+  @Test
+  public void threadPoolMonitorHasQueueSizeMeter() {
+    assertNotNull(getMeter(ThreadPoolMonitor.QUEUE_SIZE));
+  }
+
+  @Test
+  public void maxThreadsUpdatesWhenRegistryIsUpdated() {
+    final Gauge gauge = getGauge(ThreadPoolMonitor.MAX_THREADS);
+    assertEquals(10.0, gauge.value(), 0.0);
+
+    latchedExecutor.setMaximumPoolSize(42);
+    PolledMeter.update(registry);
+    assertEquals(42.0, gauge.value(), 0.0);
+  }
+
+  @Test
+  public void corePoolSizeUpdatesWhenRegistryIsUpdated() {
+    final Gauge gauge = getGauge(ThreadPoolMonitor.CORE_POOL_SIZE);
+    assertEquals(3.0, gauge.value(), 0.0);
+
+    latchedExecutor.setCorePoolSize(42);
+
+    PolledMeter.update(registry);
+    assertEquals(42.0, gauge.value(), 0.0);
+  }
+
+  @Test
+  public void taskCountUpdates() throws InterruptedException {
+    final Counter counter = getCounter(ThreadPoolMonitor.TASK_COUNT);
+    assertEquals(0, counter.count());
+
+    final CountDownLatch synchronizer = new CountDownLatch(1);
+    final CountDownLatch terminator = new CountDownLatch(1);
+    final TestRunnable command = new TestRunnable(synchronizer, terminator);
+
+    latchedExecutor.execute(command);
+
+    synchronizer.await(6, TimeUnit.SECONDS);
+    PolledMeter.update(registry);
+    assertEquals(1, counter.count(), 0.0);
+    terminator.countDown();
+  }
+
+  @Test
+  public void currentThreadsBusyCountUpdates() throws InterruptedException {
+    final Gauge gauge = getGauge(ThreadPoolMonitor.CURRENT_THREADS_BUSY);
+    assertEquals(0.0, gauge.value(), 0.0);
+
+    final CountDownLatch synchronizer = new CountDownLatch(1);
+    final CountDownLatch terminator = new CountDownLatch(1);
+    final TestRunnable command = new TestRunnable(synchronizer, terminator);
+
+    latchedExecutor.execute(command);
+
+    synchronizer.await(6, TimeUnit.SECONDS);
+    PolledMeter.update(registry);
+    assertEquals(1.0, gauge.value(), 0.0);
+
+    terminator.countDown();
+    latchedExecutor.getCompletedLatch().await(6, TimeUnit.SECONDS);
+    PolledMeter.update(registry);
+    assertEquals(0.0, gauge.value(), 0.0);
+  }
+
+  @Test
+  public void completedTaskCountUpdates() throws InterruptedException {
+    final Counter counter = getCounter(ThreadPoolMonitor.COMPLETED_TASK_COUNT);
+    assertEquals(0, counter.count());
+
+    final CountDownLatch synchronizer = new CountDownLatch(2);
+    final CountDownLatch terminator1 = new CountDownLatch(1);
+    final CountDownLatch terminator2 = new CountDownLatch(1);
+    final TestRunnable command1 = new TestRunnable(synchronizer, terminator1);
+    final TestRunnable command2 = new TestRunnable(synchronizer, terminator2);
+
+    latchedExecutor.execute(command1);
+    latchedExecutor.execute(command2);
+
+    synchronizer.await(6, TimeUnit.SECONDS);
+    PolledMeter.update(registry);
+    assertEquals(0, counter.count());
+
+    terminator1.countDown();
+    latchedExecutor.getCompletedLatch().await(6, TimeUnit.SECONDS);
+    PolledMeter.update(registry);
+    assertEquals(1, counter.count(), 0);
+
+    latchedExecutor.setCompletedLatch(new CountDownLatch(1));
+    terminator2.countDown();
+    latchedExecutor.getCompletedLatch().await(6, TimeUnit.SECONDS);
+    PolledMeter.update(registry);
+    assertEquals(2, counter.count());
+  }
+
+  @Test
+  public void poolSizeUpdates() throws InterruptedException {
+    final Gauge gauge = getGauge(ThreadPoolMonitor.POOL_SIZE);
+    assertEquals(0.0, gauge.value(), 0.0);
+
+    final CountDownLatch synchronizer = new CountDownLatch(2);
+    final CountDownLatch terminator1 = new CountDownLatch(1);
+    final CountDownLatch terminator2 = new CountDownLatch(1);
+    final TestRunnable command1 = new TestRunnable(synchronizer, terminator1);
+    final TestRunnable command2 = new TestRunnable(synchronizer, terminator2);
+
+    latchedExecutor.execute(command1);
+    latchedExecutor.execute(command2);
+
+    synchronizer.await(6, TimeUnit.SECONDS);
+    PolledMeter.update(registry);
+    assertEquals(2.0, gauge.value(), 0.0);
+
+    terminator1.countDown();
+    terminator2.countDown();
+  }
+
+  @Test
+  public void queueSizeUpdates() throws InterruptedException {
+    latchedExecutor.setCorePoolSize(1);
+    latchedExecutor.setMaximumPoolSize(1);
+    final Gauge gauge = getGauge(ThreadPoolMonitor.QUEUE_SIZE);
+    assertEquals(0.0, gauge.value(), 0.0);
+
+    final CountDownLatch synchronizer1 = new CountDownLatch(1);
+    final CountDownLatch synchronizer2 = new CountDownLatch(1);
+    final CountDownLatch terminator123 = new CountDownLatch(1);
+    final CountDownLatch terminator2 = new CountDownLatch(1);
+    final TestRunnable command1 = new TestRunnable(synchronizer1, terminator123);
+    final TestRunnable command2 = new TestRunnable(synchronizer1, terminator123);
+    final TestRunnable command3 = new TestRunnable(synchronizer1, terminator123);
+    final TestRunnable command4 = new TestRunnable(synchronizer2, terminator2);
+    final TestRunnable command5 = new TestRunnable(synchronizer2, terminator2);
+    final TestRunnable command6 = new TestRunnable(synchronizer2, terminator2);
+    final TestRunnable command7 = new TestRunnable(synchronizer2, terminator2);
+
+    latchedExecutor.execute(command1);
+    latchedExecutor.execute(command2);
+    latchedExecutor.execute(command3);
+    latchedExecutor.execute(command4);
+    latchedExecutor.execute(command5);
+    latchedExecutor.execute(command6);
+    latchedExecutor.execute(command7);
+
+    synchronizer1.await(6, TimeUnit.SECONDS);
+    PolledMeter.update(registry);
+    assertEquals(6.0, gauge.value(), 0.0);
+
+    latchedExecutor.setCompletedLatch(new CountDownLatch(3));
+    terminator123.countDown();
+    latchedExecutor.getCompletedLatch().await(6, TimeUnit.SECONDS);
+
+    synchronizer2.await(6, TimeUnit.SECONDS);
+    PolledMeter.update(registry);
+    assertEquals(3.0, gauge.value(), 0.0);
+
+    terminator2.countDown();
+  }
+}
