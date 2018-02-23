2d9330ce7557996b71408d54401f240238de0e3b
brharrington <brharrington@gmail.com>
2018-02-22 20:38:35 -0800

    update nflx plugin to use archaius2 (#525)
    Runtime is trying to phase out the usage of archaius1.

--- spectator-nflx-plugin/src/main/java/com/netflix/spectator/nflx/SpectatorModule.java
+++ spectator-nflx-plugin/src/main/java/com/netflix/spectator/nflx/SpectatorModule.java
@@ -15,17 +15,21 @@
  */
 package com.netflix.spectator.nflx;
 
+import com.google.inject.Inject;
 import com.google.inject.multibindings.OptionalBinder;
+import com.netflix.archaius.api.Config;
+import com.netflix.archaius.config.EmptyConfig;
 import com.netflix.spectator.servo.ServoRegistry;
 
 import javax.annotation.PreDestroy;
-import javax.inject.Inject;
 import javax.inject.Provider;
 
 import com.google.inject.AbstractModule;
 import com.netflix.spectator.api.ExtendedRegistry;
 import com.netflix.spectator.api.Registry;
 import com.netflix.spectator.api.Spectator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Guice module to configure the appropriate bindings for running an application. Note that this
@@ -61,8 +65,11 @@ import com.netflix.spectator.api.Spectator;
  * </pre>
  */
 public final class SpectatorModule extends AbstractModule {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(SpectatorModule.class);
+
   @Override protected void configure() {
-    bind(Plugin.class).asEagerSingleton();
+    bind(Plugin.class).toProvider(PluginProvider.class).asEagerSingleton();
     bind(StaticManager.class).asEagerSingleton();
     OptionalBinder.newOptionalBinder(binder(), ExtendedRegistry.class)
         .setDefault()
@@ -79,6 +86,32 @@ public final class SpectatorModule extends AbstractModule {
 
   @Override public int hashCode() {
     return getClass().hashCode();
+  }
+
+  private static class PluginProvider implements Provider<Plugin> {
+    private final Plugin plugin;
+
+    @Inject
+    PluginProvider(Registry registry, OptionalInjections opts) {
+      plugin = new Plugin(registry, opts.config());
+    }
+
+    @Override public Plugin get() {
+      return plugin;
+    }
+  }
+
+  private static class OptionalInjections {
+    @Inject(optional = true)
+    private Config config;
+
+    Config config() {
+      if (config == null) {
+        LOGGER.warn("no archaius2 binding found, using empty configuration");
+        config = EmptyConfig.INSTANCE;
+      }
+      return config;
+    }
   }
 
   private static class StaticManager {
