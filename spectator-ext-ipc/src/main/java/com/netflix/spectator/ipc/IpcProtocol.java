de97f0c756410be313c89443138244b25a7912b6
Brian Harrington <brharrington@gmail.com>
2018-05-22 13:11:27 -0700

    add enum for standard protocols
    The values used for HTTP requests should always and are
    fairly standard.

---
+++ spectator-ext-ipc/src/main/java/com/netflix/spectator/ipc/IpcProtocol.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2014-2018 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.ipc;
+
+import com.netflix.spectator.api.Tag;
+
+/**
+ * Dimension indicating the protocol. This enum includes the standard values for well
+ * established protocols that should agree across all implementations.
+ */
+public enum IpcProtocol implements Tag {
+  /** HTTP 1.x. */
+  http_1,
+
+  /** HTTP 2.x. */
+  http_2;
+
+  @Override public String key() {
+    return IpcTagKey.protocol.key();
+  }
+
+  @Override public String value() {
+    return name();
+  }
+}
