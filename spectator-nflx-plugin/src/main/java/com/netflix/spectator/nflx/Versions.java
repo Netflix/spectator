9f1ec79158a472d0befb043604d4d59f6819c149
Brian Harrington <brharrington@gmail.com>
2018-05-29 13:43:56 -0700

    use AtlasRegistry if atlas-client is new enough
    To enable a smooth transition from ServoRegistry to
    AtlasRegistry for internal users, this change detects
    whether a new class in the internal client is present
    and sets the registry accordingly. This should allow
    us to release the newer version of Spectator and have
    some users pick it up before the new version of the
    client is released.

---
+++ spectator-nflx-plugin/src/main/java/com/netflix/spectator/nflx/Versions.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014-2018 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.nflx;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Checks that the versions for servo and atlas-client are new enough to support using
+ * the AtlasRegistry and avoiding servo. Most apps should pick up the new versions via
+ * recommendations, but there is likely to be some mismatch for a bit. This should allow
+ * a more graceful transition as it will fallback to the previous behavior if the other
+ * associated libraries are not new enough.
+ */
+class Versions {
+
+  private static Logger LOGGER = LoggerFactory.getLogger(Versions.class);
+
+  /**
+   * If the atlas-client version is new enough, then this class will be present in the
+   * classpath to poll the data from Spectator rather than Servo.
+   */
+  private static final String ATLAS_CLIENT_CLASS = "com.netflix.atlas.plugin.SpectatorMetricPoller";
+
+  /** Check if a class is on the classpath. */
+  private static boolean isClassPresent(String className) {
+    try {
+      Class.forName(className, false, Versions.class.getClassLoader());
+      LOGGER.debug("class {} found in classpath", className);
+      return true;
+    } catch (Throwable t) {
+      LOGGER.debug("failed to load class: {}", className, t);
+      return false;
+    }
+  }
+
+  /**
+   * Returns true if the atlas-client version is new enough to use AtlasRegistry instead
+   * of ServoRegistry.
+   */
+  static boolean useAtlasRegistry() {
+    return isClassPresent(ATLAS_CLIENT_CLASS);
+  }
+
+}
