Netflix/spectator

commit 13e1e6bb79149bcf71e5664b29983e5b4112b902

support traversal of ids without allocations (#722)
Adds a base interface for Id that allows it to be treated
consistently as a collection of tags, i.e., name is not
special other than being the first entry. It also allows
the tag keys and values to be accessed without allocating
a Tag object.

spectator-api/src/main/java/com/netflix/spectator/api/Id.java
@@ -20,7 +20,8 @@ import java.util.Map;
 /**
  * Identifier for a meter or measurement.
  */
-public interface Id {
+public interface Id extends TagList {
+
   /** Description of the measurement that is being collected. */
   String name();
 

build.gradle
@@ -100,7 +100,7 @@ subprojects {
     profilers = ['stack', 'gc']
     includeTests = false
     duplicateClassesStrategy = 'warn'
-    include = ['.*PercentileTimers.*']
+    include = ['.*IdTraversal.*']
   }
 
   checkstyle {

spectator-api/src/jmh/java/com/netflix/spectator/perf/IdTraversal.java
@@ -1,0 +1,75 @@
+/*
+ * Copyright 2014-2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.perf;
+
+import com.netflix.spectator.api.Id;
+import com.netflix.spectator.api.Tag;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+/**
+ * <pre>
+ * Benchmark                             Mode  Cnt        Score        Error   Units
+ * IdTraversal.forEach                  thrpt   10  9804067.543 ±  70757.222   ops/s
+ * IdTraversal.iterator                 thrpt   10  6431905.789 ± 814667.475   ops/s
+ * </pre>
+ *
+ * In an actual app we measured a big difference in allocations for the Tag objects. For
+ * this test hotspot seems to be able to avoid most of them:
+ *
+ * https://shipilev.net/jvm/anatomy-quarks/18-scalar-replacement/
+ *
+ * <pre>
+ * Benchmark                             Mode  Cnt        Score        Error   Units
+ * IdTraversal.forEach     gc.alloc.rate.norm   10        0.039 ±      0.001    B/op
+ * IdTraversal.iterator    gc.alloc.rate.norm   10        0.059 ±      0.006    B/op
+ * </pre>
+ */
+@State(Scope.Thread)
+public class IdTraversal {
+
+  private final Id baseId = Id.create("http.req.complete")
+      .withTag(    "nf.app", "test_app")
+      .withTag("nf.cluster", "test_app-main")
+      .withTag(    "nf.asg", "test_app-main-v042")
+      .withTag(  "nf.stack", "main")
+      .withTag(    "nf.ami", "ami-0987654321")
+      .withTag( "nf.region", "us-east-1")
+      .withTag(   "nf.zone", "us-east-1e")
+      .withTag(   "nf.node", "i-1234567890")
+      .withTag(   "country", "US")
+      .withTag(    "device", "xbox")
+      .withTag(    "status", "200")
+      .withTag(    "client", "ab");
+
+  @Benchmark
+  public void iterator(Blackhole bh) {
+    for (Tag t : baseId) {
+      bh.consume(t.key());
+      bh.consume(t.value());
+    }
+  }
+
+  @Benchmark
+  public void forEach(Blackhole bh) {
+    baseId.forEach((k, v) -> {
+      bh.consume(k);
+      bh.consume(v);
+    });
+  }
+}

spectator-api/src/main/java/com/netflix/spectator/api/ArrayTagSet.java
@@ -19,15 +19,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 /**
  * An immutable set of tags sorted by the tag key.
  */
-final class ArrayTagSet implements Iterable<Tag> {
+final class ArrayTagSet implements TagList {
 
   private static final Comparator<Tag> TAG_COMPARATOR = (t1, t2) -> t1.key().compareTo(t2.key());
 
@@ -73,25 +71,6 @@ final class ArrayTagSet implements Iterable<Tag> {
     this.tags = tags;
     this.length = length;
     this.cachedHashCode = 0;
-  }
-
-  @Override public Iterator<Tag> iterator() {
-    return new Iterator<Tag>() {
-      private int i = 0;
-
-      @Override public boolean hasNext() {
-        return i < length;
-      }
-
-      @Override public Tag next() {
-        if (i >= length) {
-          throw new NoSuchElementException("next called after end of iterator");
-        }
-        final String k = tags[i++];
-        final String v = tags[i++];
-        return new BasicTag(k, v);
-      }
-    };
   }
 
   /** Check if this set is empty. */
@@ -321,8 +300,18 @@ final class ArrayTagSet implements Iterable<Tag> {
     }
   }
 
+  /** Return the key at the specified position. */
+  @Override public String getKey(int i) {
+    return tags[i * 2];
+  }
+
+  /** Return the value at the specified position. */
+  @Override public String getValue(int i) {
+    return tags[i * 2 + 1];
+  }
+
   /** Return the current size of this tag set. */
-  public int size() {
+  @Override public int size() {
     return length / 2;
   }
 

spectator-api/src/main/java/com/netflix/spectator/api/DefaultId.java
@@ -71,6 +71,18 @@ final class DefaultId implements Id {
     return new DefaultId(name, tags.addAll(ts));
   }
 
+  @Override public String getKey(int i) {
+    return i == 0 ? "name" : tags.getKey(i - 1);
+  }
+
+  @Override public String getValue(int i) {
+    return i == 0 ? name : tags.getValue(i - 1);
+  }
+
+  @Override public int size() {
+    return tags.size() + 1;
+  }
+
   /**
    * Returns a new id with the tag list sorted by key and with no duplicate keys.  This is equivalent to
    * {@code rollup(Collections.<String>emptySet(), false)}.

spectator-api/src/main/java/com/netflix/spectator/api/NoopId.java
@@ -53,4 +53,16 @@ final class NoopId implements Id {
   @Override public String toString() {
     return name();
   }
+
+  @Override public String getKey(int i) {
+    return "name";
+  }
+
+  @Override public String getValue(int i) {
+    return name();
+  }
+
+  @Override public int size() {
+    return 1;
+  }
 }

spectator-api/src/main/java/com/netflix/spectator/api/TagList.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright 2014-2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spectator.api;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.BiConsumer;
+
+/**
+ * Base type for a collection of tags. Allows access to the keys and values without allocations
+ * if the underlying implementation does not store them as Tag objects.
+ */
+public interface TagList extends Iterable<Tag> {
+
+  /** Return the key at the specified index. */
+  String getKey(int i);
+
+  /** Return the value at the specified index. */
+  String getValue(int i);
+
+  /** Return the number of tags in the list. */
+  int size();
+
+  /** Return the tag at the specified index. */
+  default Tag getTag(int i) {
+    return Tag.of(getKey(i), getValue(i));
+  }
+
+  /** Apply the consumer function for each tag in the list. */
+  default void forEach(BiConsumer<String, String> consumer) {
+    final int n = size();
+    for (int i = 0; i < n; ++i) {
+      consumer.accept(getKey(i), getValue(i));
+    }
+  }
+  
+  @Override default Iterator<Tag> iterator() {
+    final int length = size();
+    return new Iterator<Tag>() {
+      private int i = 0;
+
+      @Override public boolean hasNext() {
+        return i < length;
+      }
+
+      @Override public Tag next() {
+        if (i >= length) {
+          throw new NoSuchElementException("next called after end of iterator");
+        }
+        return getTag(i++);
+      }
+    };
+  }
+}

spectator-api/src/test/java/com/netflix/spectator/api/ArrayTagSetTest.java
@@ -338,4 +338,24 @@ public class ArrayTagSetTest {
         .add(new BasicTag("b", "1"));
     Assertions.assertEquals(expected, ts);
   }
+
+  @Test
+  public void tagListIterate() {
+    ArrayTagSet expected = ArrayTagSet.create("a", "1", "b", "2", "c", "3", "d", "4", "e", "5");
+    ArrayTagSet actual = ArrayTagSet.EMPTY;
+    for (Tag t : expected) {
+      actual = actual.add(t);
+    }
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void tagListForEach() {
+    ArrayTagSet expected = ArrayTagSet.create("a", "1", "b", "2", "c", "3", "d", "4", "e", "5");
+    List<Tag> tmp = new ArrayList<>();
+    expected.forEach((k, v) -> {
+      tmp.add(Tag.of(k, v));
+    });
+    Assertions.assertEquals(expected, ArrayTagSet.create(tmp));
+  }
 }

commit 8ac07d59f79e0772ed5324aa8a6b205de4188e16

add default implementations of new methods to Id
Though not recommended, in some cases users are implementing
the `Id` interface rather than using `registry.createId`. This
change adds some default implementations to avoid breaking
older code for changes introduced in #722. It also updates the
base registry implementation to normalize the ids to force them
to be a proper implementation created by the registry and avoid
confusing behavior.

spectator-api/src/main/java/com/netflix/spectator/api/AbstractRegistry.java
@@ -133,6 +133,17 @@ public abstract class AbstractRegistry implements Registry {
     return new DefaultId(name, ArrayTagSet.create(tags));
   }
 
+  /**
+   * Ensure a the id type is correct. While not recommended, nothing stops users from using a
+   * custom implementation of the {@link Id} interface. This can create unexpected and strange
+   * problems with the lookups failing, duplicate counters, etc. To avoid issues, this method
+   * should be called to sanitize Id values coming from the user. If it is already a valid id,
+   * then it will not create a new instance.
+   */
+  private Id normalizeId(Id id) {
+    return (id instanceof DefaultId) ? id : createId(id.name(), id.tags());
+  }
+
   private void logTypeError(Id id, Class<?> desired, Class<?> found) {
     final String dtype = desired.getName();
     final String ftype = found.getName();
@@ -154,32 +165,37 @@ public abstract class AbstractRegistry implements Registry {
   }
 
   @Override public final Counter counter(Id id) {
-    Counter c = getOrCreate(id, Counter.class, NoopCounter.INSTANCE, this::newCounter);
-    return new SwapCounter(this, id, c);
+    Id normId = normalizeId(id);
+    Counter c = getOrCreate(normId, Counter.class, NoopCounter.INSTANCE, this::newCounter);
+    return new SwapCounter(this, normId, c);
   }
 
   @Override public final DistributionSummary distributionSummary(Id id) {
+    Id normId = normalizeId(id);
     DistributionSummary ds = getOrCreate(
-        id,
+        normId,
         DistributionSummary.class,
         NoopDistributionSummary.INSTANCE,
         this::newDistributionSummary);
-    return new SwapDistributionSummary(this, id, ds);
+    return new SwapDistributionSummary(this, normId, ds);
   }
 
   @Override public final Timer timer(Id id) {
-    Timer t = getOrCreate(id, Timer.class, NoopTimer.INSTANCE, this::newTimer);
-    return new SwapTimer(this, id, t);
+    Id normId = normalizeId(id);
+    Timer t = getOrCreate(normId, Timer.class, NoopTimer.INSTANCE, this::newTimer);
+    return new SwapTimer(this, normId, t);
   }
 
   @Override public final Gauge gauge(Id id) {
-    Gauge g = getOrCreate(id, Gauge.class, NoopGauge.INSTANCE, this::newGauge);
-    return new SwapGauge(this, id, g);
+    Id normId = normalizeId(id);
+    Gauge g = getOrCreate(normId, Gauge.class, NoopGauge.INSTANCE, this::newGauge);
+    return new SwapGauge(this, normId, g);
   }
 
   @Override public final Gauge maxGauge(Id id) {
-    Gauge g = getOrCreate(id, Gauge.class, NoopGauge.INSTANCE, this::newMaxGauge);
-    return new SwapMaxGauge(this, id, g);
+    Id normId = normalizeId(id);
+    Gauge g = getOrCreate(normId, Gauge.class, NoopGauge.INSTANCE, this::newMaxGauge);
+    return new SwapMaxGauge(this, normId, g);
   }
 
   /**
@@ -217,7 +233,7 @@ public abstract class AbstractRegistry implements Registry {
   }
 
   @Override public final Meter get(Id id) {
-    return meters.get(id);
+    return meters.get(normalizeId(id));
   }
 
   @Override public final Iterator<Meter> iterator() {

spectator-api/src/main/java/com/netflix/spectator/api/Id.java
@@ -138,6 +138,21 @@ public interface Id extends TagList {
     return tmp;
   }
 
+  /** Return the key at the specified index. The name will be treated as position 0. */
+  @Override default String getKey(int i) {
+    return i == 0 ? "name" : Utils.getValue(tags(), i - 1).key();
+  }
+
+  /** Return the value at the specified index. The name will be treated as position 0. */
+  @Override default String getValue(int i) {
+    return i == 0 ? name() : Utils.getValue(tags(), i - 1).value();
+  }
+
+  /** Return the size, number of tags, for the id including the name. */
+  @Override default int size() {
+    return Utils.size(tags()) + 1;
+  }
+
   /**
    * Create an immutable Id with the provided name. In many cases it is preferable to use
    * {@link Registry#createId(String)} instead so that the overhead for instrumentation can

spectator-api/src/main/java/com/netflix/spectator/api/Utils.java
@@ -122,6 +122,33 @@ public final class Utils {
   }
 
   /**
+   * Return the value at a given position in the iterable.
+   *
+   * @param values
+   *     A collection of values. For deterministic behavior the iteration order of the
+   *     container type must be consistent each time.
+   * @param pos
+   *     Position of the value to extract.
+   * @return
+   *     Value at the given position.
+   */
+  public static <T> T getValue(Iterable<T> values, int pos) {
+    if (values instanceof List) {
+      List<T> vs = (List<T>) values;
+      return vs.get(pos);
+    } else {
+      int i = 0;
+      for (T v : values) {
+        if (i == pos) {
+          return v;
+        }
+        ++i;
+      }
+      throw new IndexOutOfBoundsException(pos + " >= " + i);
+    }
+  }
+
+  /**
    * Returns the first measurement with a given tag value.
    *
    * @param ms
@@ -241,8 +268,8 @@ public final class Utils {
    */
   @SuppressWarnings("PMD.UnusedLocalVariable")
   public static <T> int size(Iterable<T> iter) {
-    if (iter instanceof ArrayTagSet) {
-      return ((ArrayTagSet) iter).size();
+    if (iter instanceof TagList) {
+      return ((TagList) iter).size();
     } else if (iter instanceof Collection<?>) {
       return ((Collection<?>) iter).size();
     } else {

spectator-api/src/test/java/com/netflix/spectator/api/RegistryTest.java
@@ -20,8 +20,11 @@ import com.netflix.spectator.impl.SwapMeter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.DoubleSummaryStatistics;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.LongSummaryStatistics;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -573,5 +576,114 @@ public class RegistryTest {
     PolledMeter.update(r);
     Assertions.assertEquals(0, r.stream().count());
     Assertions.assertEquals(0, r.state().size());
+  }
+
+  @Test
+  public void customIdTags() {
+    DefaultRegistry r = newRegistry(false, 10000);
+    Id id = new CustomId("test").withTags("b", "2", "a", "1");
+    r.counter(id).increment();
+    Assertions.assertEquals(1, r.counter("test", "a", "1", "b", "2").count());
+  }
+
+  @Test
+  public void customIdCounter() {
+    DefaultRegistry r = newRegistry(false, 10000);
+    Id id = new CustomId("test");
+    r.counter(id).increment();
+    Assertions.assertEquals(1, r.counter("test").count());
+  }
+
+  @Test
+  public void customIdGauge() {
+    DefaultRegistry r = newRegistry(false, 10000);
+    Id id = new CustomId("test");
+    r.gauge(id).set(42.0);
+    Assertions.assertEquals(42.0, r.gauge("test").value(), 1e-12);
+  }
+
+  @Test
+  public void customIdMaxGauge() {
+    DefaultRegistry r = newRegistry(false, 10000);
+    Id id = new CustomId("test");
+    r.maxGauge(id).set(42.0);
+    Assertions.assertEquals(42.0, r.maxGauge("test").value(), 1e-12);
+  }
+
+  @Test
+  public void customIdTimer() {
+    DefaultRegistry r = newRegistry(false, 10000);
+    Id id = new CustomId("test");
+    r.timer(id).record(42, TimeUnit.NANOSECONDS);
+    Assertions.assertEquals(42L, r.timer("test").totalTime());
+  }
+
+  @Test
+  public void customIdDistributionSummary() {
+    DefaultRegistry r = newRegistry(false, 10000);
+    Id id = new CustomId("test");
+    r.distributionSummary(id).record(42);
+    Assertions.assertEquals(42L, r.distributionSummary("test").totalAmount());
+  }
+
+  @Test
+  public void customIdGetKey() {
+    Id id = new CustomId("test").withTags("b", "2", "a", "1");
+    Assertions.assertEquals("name", id.getKey(0));
+    Assertions.assertEquals("b", id.getKey(1));
+    Assertions.assertEquals("a", id.getKey(2));
+  }
+
+  @Test
+  public void customIdGetValue() {
+    Id id = new CustomId("test").withTags("b", "2", "a", "1");
+    Assertions.assertEquals("test", id.getValue(0));
+    Assertions.assertEquals("2", id.getValue(1));
+    Assertions.assertEquals("1", id.getValue(2));
+  }
+
+  @Test
+  public void customIdSize() {
+    Id id = new CustomId("test").withTags("b", "2", "a", "1");
+    Assertions.assertEquals(3, id.size());
+  }
+
+  // Used to test that custom Id implementations will get normalized correctly and that
+  // backwards compatibility for inheritance is maintained.
+  public static class CustomId implements Id {
+
+    private final String name;
+    private final List<Tag> tags;
+
+    CustomId(String name) {
+      this(name, Collections.emptyList());
+    }
+
+    CustomId(String name, List<Tag> tags) {
+      this.name = name;
+      this.tags = Collections.unmodifiableList(tags);
+    }
+
+    private List<Tag> append(Tag tag) {
+      List<Tag> tmp = new ArrayList<>(tags);
+      tmp.add(tag);
+      return tmp;
+    }
+
+    @Override public String name() {
+      return name;
+    }
+
+    @Override public Iterable<Tag> tags() {
+      return tags;
+    }
+
+    @Override public Id withTag(String k, String v) {
+      return new CustomId(name, append(Tag.of(k, v)));
+    }
+
+    @Override public Id withTag(Tag t) {
+      return new CustomId(name, append(t));
+    }
   }
 }

spectator-api/src/test/java/com/netflix/spectator/api/UtilsTest.java
@@ -111,4 +111,54 @@ public class UtilsTest {
     List<Measurement> out = Utils.toList(Utils.filter(ms, v -> false));
     Assertions.assertEquals(0, out.size());
   }
+
+  @Test
+  public void sizeTagList() {
+    Id id = Id.create("test").withTags("a", "1", "b", "2");
+    Assertions.assertEquals(2, Utils.size(id.tags()));
+  }
+
+  @Test
+  public void sizeCollection() {
+    List<String> vs = new ArrayList<>();
+    vs.add("a");
+    vs.add("b");
+    vs.add("c");
+    Assertions.assertEquals(3, Utils.size(vs));
+  }
+
+  @Test
+  public void sizeIterable() {
+    List<String> vs = new ArrayList<>();
+    vs.add("a");
+    vs.add("b");
+    Assertions.assertEquals(2, Utils.size(vs::iterator));
+  }
+
+  @Test
+  public void getValueList() {
+    List<String> vs = new ArrayList<>();
+    vs.add("a");
+    vs.add("b");
+    vs.add("c");
+    Assertions.assertEquals("a", Utils.getValue(vs, 0));
+    Assertions.assertEquals("b", Utils.getValue(vs, 1));
+    Assertions.assertEquals("c", Utils.getValue(vs, 2));
+    Assertions.assertThrows(IndexOutOfBoundsException.class, () -> Utils.getValue(vs, 3));
+    Assertions.assertThrows(IndexOutOfBoundsException.class, () -> Utils.getValue(vs, -3));
+  }
+
+  @Test
+  public void getValueIterable() {
+    List<String> vs = new ArrayList<>();
+    vs.add("a");
+    vs.add("b");
+    vs.add("c");
+    Iterable<String> it = vs::iterator;
+    Assertions.assertEquals("a", Utils.getValue(it, 0));
+    Assertions.assertEquals("b", Utils.getValue(it, 1));
+    Assertions.assertEquals("c", Utils.getValue(it, 2));
+    Assertions.assertThrows(IndexOutOfBoundsException.class, () -> Utils.getValue(it, 3));
+    Assertions.assertThrows(IndexOutOfBoundsException.class, () -> Utils.getValue(it, -3));
+  }
 }