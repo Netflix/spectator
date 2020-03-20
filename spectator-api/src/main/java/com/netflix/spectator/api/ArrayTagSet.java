d2e66a8ade925b1deda7b59c9acb56f1f68ab297
Brian Harrington <brharrington@gmail.com>
2020-03-20 09:26:01 -0500

    avoid * imports

--- spectator-api/src/main/java/com/netflix/spectator/api/ArrayTagSet.java
+++ spectator-api/src/main/java/com/netflix/spectator/api/ArrayTagSet.java
@@ -17,7 +17,11 @@ package com.netflix.spectator.api;
 
 import com.netflix.spectator.impl.Preconditions;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 
 /**
