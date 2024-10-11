17e6d74bca534e8179eeb3bcfef7944c776154e8
Patrick Strawderman <pstrawderman@netflix.com>
2024-10-11 08:04:20 -0700

    reduce memory overhead for step value types (#1158)
    Use AtomicLongFieldUpdater to shrink the footprint of StepLong, StepDouble,
    and AtomicDouble. This avoids the object overhead and pointer traversal
    needed when using AtomicLong internally. 
    
    Note, it does change some of the public methods on StepLong and StepDouble.
    However, those are internal implementation classes and should not be used
    outside of the spectator libraries.
    
    ---------
    
    Co-authored-by: Brian Harrington <brharrington@gmail.com>

--- spectator-reg-atlas/src/main/java/com/netflix/spectator/atlas/AtlasCounter.java
+++ spectator-reg-atlas/src/main/java/com/netflix/spectator/atlas/AtlasCounter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2020 Netflix, Inc.
+ * Copyright 2014-2024 Netflix, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,8 +47,8 @@ class AtlasCounter extends AtlasMeter implements Counter {
 
   @Override public void add(double amount) {
     if (Double.isFinite(amount) && amount > 0.0) {
-      long now = clock.wallTime();
-      value.getCurrent(now).addAndGet(amount);
+      final long now = clock.wallTime();
+      value.addAndGet(now, amount);
       updateLastModTime(now);
     }
   }
