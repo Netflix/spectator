a4ccb26a63365511280c2a106adb0eb405256909
Filip Zybała <fzybala@netflix.com>
2026-06-23 20:06:45 +0200

    reformat CompositeAutoCloseable.java

--- spectator-ext-jvm/src/main/java/com/netflix/spectator/jvm/CompositeAutoCloseable.java
+++ spectator-ext-jvm/src/main/java/com/netflix/spectator/jvm/CompositeAutoCloseable.java
@@ -4,26 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 class CompositeAutoCloseable implements AutoCloseable {
-    private final List<AutoCloseable> autoCloseables = new ArrayList<>();
+  private final List<AutoCloseable> autoCloseables = new ArrayList<>();
 
-    public void add(AutoCloseable autoCloseable) {
-        autoCloseables.add(autoCloseable);
-    }
+  public void add(AutoCloseable autoCloseable) {
+    autoCloseables.add(autoCloseable);
+  }
 
-    @Override
-    public void close() throws Exception {
-        Exception firstException = null;
-        for (AutoCloseable autoCloseable : autoCloseables) {
-            try {
-                autoCloseable.close();
-            } catch (Exception e) {
-                if (firstException != null) {
-                    firstException.addSuppressed(e);
-                } else {
-                    firstException = e;
-                }
-            }
+  @Override
+  public void close() throws Exception {
+    Exception firstException = null;
+    for (AutoCloseable autoCloseable : autoCloseables) {
+      try {
+        autoCloseable.close();
+      } catch (Exception e) {
+        if (firstException != null) {
+          firstException.addSuppressed(e);
+        } else {
+          firstException = e;
         }
-        if (firstException != null) throw firstException;
+      }
     }
+    if (firstException != null) throw firstException;
+  }
 }
